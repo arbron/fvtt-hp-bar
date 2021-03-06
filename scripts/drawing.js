29e20d1c531aa80af50a2aeb06ea75e99afe1ed6
Jeff Hitchcock <mail@command-center.org>
2021-03-06 10:39:45 -0800

    Add line to indicate max HP reductions, adjust temp HP background spacing

--- scripts/drawing.js
+++ scripts/drawing.js
@@ -22,26 +22,40 @@ export function drawHPBar(number, bar, data) {
 
   const tempColor = 0x559cc6;
   const valueColor = [(1-(valueColorPct/2)), valueColorPct, 0];
-  const maxColor = (hp.tempmax > 0) ? 0xf4f4f4 : 0x999999;
+  const maxBackgroundColor = (hp.tempmax > 0) ? 0xf4f4f4 : 0x999999;
+  const maxLineColor = 0xf4f4f4;
 
   const w = this.w;
   let h = Math.max((canvas.dimensions.size / 12), 8);
   if ( this.data.height >= 2 ) h *= 1.6;  // Enlarge the bar for large tokens
 
-  // Draw the bar
+  // Background
   bar.clear()
      .beginFill(0x000000, 0.5)
      .lineStyle(2, 0.000000, 0.9)
-     .drawRoundedRect(0, 0, w, h, 3)  // Background
-     .beginFill(maxColor, 0.5)
-     .lineStyle(0, 0x000000, 0.0)
-     .drawRoundedRect((1-maxPct)*(w-2), 1, maxPct*(w-2), h-2, 2)  // Max HP Bar
-     .beginFill(PIXI.utils.rgb2hex(valueColor), 0.8)
+     .drawRoundedRect(0, 0, w, h, 3)
+
+  // Temp Max Background
+  if (hp.tempmax != 0) {
+    bar.beginFill(maxBackgroundColor, 0.5)
+       .lineStyle(0, 0x000000, 0.0)
+       .drawRoundedRect((1-maxPct)*(w-2), 1, maxPct*w, h-2, 2)
+  }
+
+  // Current & Temp Bars
+  bar.beginFill(PIXI.utils.rgb2hex(valueColor), 0.8)
      .lineStyle(1, 0x000000, 0.8)
-     .drawRoundedRect(1, 1, valuePct*(w-2), h-2, 2)  // Current HP Bar
-     .beginFill(tempColor, 0.6)
+     .drawRoundedRect(1, 1, valuePct*(w-2), h-2, 2)
+     .beginFill(tempColor, 0.8)
      .lineStyle(1, 0x000000, 0.4)
-     .drawRoundedRect(1, 1, tempPct*(w-2), h-2, 2);  // Temp HP Bar
+     .drawRoundedRect(1, 1, tempPct*(w-2), h-2, 2)
+
+  // Negative temp max line
+  if (hp.tempmax < 0) {
+    bar.lineStyle(2, maxLineColor, 0.8)
+       .moveTo((1-maxPct)*(w-2), -1)
+       .lineTo((1-maxPct)*(w-2), h+1);
+  }
 
   // Set position
   let posY = number === 0 ? this.h - h : 0;
