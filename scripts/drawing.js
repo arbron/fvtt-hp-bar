444b5d2c90d040722326f0aa8d52f039c99f73ab
Jeff Hitchcock <mail@command-center.org>
2021-03-11 13:18:58 -0800

    Update 5e to shared drawing system

--- scripts/drawing.js
+++ scripts/drawing.js
@@ -16,28 +16,42 @@ export class Draw {
   }
 
   background() {
-    this.bar.beginFill(0x000000, 0.5)
-            .lineStyle(2, 0.000000, 0.9)
+    this.bar.beginFill(Color.black, 0.5)
+            .lineStyle(2, Color.black, 0.9)
             .drawRoundedRect(0, 0, this.w, this.h, 3);
     return this;
   }
 
   current(pct, color) {
     this.bar.beginFill(color, 0.8)
-            .lineStyle(1, 0x000000, 0.8)
+            .lineStyle(1, Color.black, 0.8)
             .drawRoundedRect(1, 1, pct*(this.w-2), this.h-2, 2);
     return this;
   }
 
   temp(pct, color=Color.temp) {
     this.bar.beginFill(color, 0.8)
-            .lineStyle(1, 0x000000, 0.4)
+            .lineStyle(1, Color.black, 0.4)
             .drawRoundedRect(1, 1, pct*(this.w-2), this.h-2, 2);
     return this;
   }
 
+  fill(pct, color) {
+    this.bar.beginFill(color, 0.5)
+            .lineStyle(0, 0x000000, 0.0)
+            .drawRoundedRect((pct)*(this.w-2), 1, (1-pct)*this.w, this.h-2, 2)
+    return this;
+  }
+
+  line(pct, color) {
+    this.bar.lineStyle(2, color, 0.8)
+            .moveTo((pct)*(this.w-2), -1)
+            .lineTo((pct)*(this.w-2), this.h+1);
+    return this;
+  }
+
   outerBorder(pct, color) {
-    this.bar.beginFill(0x000000, 0.0)
+    this.bar.beginFill(Color.black, 0.0)
             .lineStyle(2, color, 0.7)
             .drawRoundedRect(0, 0, pct*this.w, this.h, 3);
     return this;
--- scripts/systems/dnd5e.js
+++ scripts/systems/dnd5e.js
@@ -1,3 +1,6 @@
+import { Color, Draw } from '../drawing.js';
+
+
 export function shouldDrawHPBar(attribute) {
   return attribute === "attributes.hp";
 }
@@ -26,42 +29,25 @@ export function drawHPBar(number, bar, data) {
   const maxPct = Math.clamped(positiveMax - Math.abs(hp.tempmax), 0, positiveMax) / size;
   const valueColorPct = Math.clamped(hp.value, 0, currentMax) / currentMax;
 
-  const tempColor = 0x559cc6;
-  const valueColor = [(1-(valueColorPct/2)), valueColorPct, 0];
   const maxBackgroundColor = (hp.tempmax > 0) ? 0xf4f4f4 : 0x999999;
-  const maxLineColor = 0xf4f4f4;
 
   const w = this.w;
   let h = Math.max((canvas.dimensions.size / 12), 8);
   if ( this.data.height >= 2 ) h *= 1.6;  // Enlarge the bar for large tokens
 
-  // Background
-  bar.clear()
-     .beginFill(0x000000, 0.5)
-     .lineStyle(2, 0.000000, 0.9)
-     .drawRoundedRect(0, 0, w, h, 3)
+  const draw = new Draw(bar, this.w, h);
+  draw.background();
 
   // Temp Max Background
-  if (hp.tempmax != 0) {
-    bar.beginFill(maxBackgroundColor, 0.5)
-       .lineStyle(0, 0x000000, 0.0)
-       .drawRoundedRect((maxPct)*(w-2), 1, (1-maxPct)*w, h-2, 2)
-  }
+  if (hp.tempmax != 0)
+    draw.fill(maxPct, maxBackgroundColor);
 
-  // Current & Temp Bars
-  bar.beginFill(PIXI.utils.rgb2hex(valueColor), 0.8)
-     .lineStyle(1, 0x000000, 0.8)
-     .drawRoundedRect(1, 1, valuePct*(w-2), h-2, 2)
-     .beginFill(tempColor, 0.8)
-     .lineStyle(1, 0x000000, 0.4)
-     .drawRoundedRect(1, 1, tempPct*(w-2), h-2, 2)
+  draw.current(valuePct, Color.forValue(valueColorPct))
+      .temp(tempPct);
 
   // Negative temp max line
-  if (hp.tempmax < 0) {
-    bar.lineStyle(2, maxLineColor, 0.8)
-       .moveTo((maxPct)*(w-2), -1)
-       .lineTo((maxPct)*(w-2), h+1);
-  }
+  if (hp.tempmax < 0)
+    draw.line(maxPct, 0xf4f4f4);
 
   // Set position
   let posY = number === 0 ? this.h - h : 0;
