4e369b16eb577e5379d673602220495cd8b35a2e
mclemente <matheus.xb@gmail.com>
2021-05-20 21:41:43 -0300

    Tormenta20 Support

---
+++ scripts/systems/tormenta20.js
@@ -0,0 +1,40 @@
+import HPBarBase from './default.js';
+import { Color } from '../drawing.js';
+
+
+export default class Tormenta20Bar extends HPBarBase {
+  /** @inheritdoc */
+  static get themeOptions() {
+    return [
+      ...super.themeOptions,
+      HPBarBase._defaultTempTheme,
+      HPBarBase._defaultMaxTheme()
+    ];
+  }
+
+  /** @inheritdoc */
+	static shouldDraw(attribute) {
+    return attribute === "attributes.pv";
+  }
+	
+	/** @inheritdoc */
+  draw(draw) {
+    const _hp = duplicate(this.data.attributes.pv);
+    const hp = {
+      max: Number(_hp.max),
+      temp: Number(_hp.temp),
+      value: Number(_hp.value),
+    }
+
+    const size = Math.max(hp.max, hp.temp);
+  
+    const tempPct = Math.clamped(hp.temp, 0, size) / size;
+    const valuePct = Math.clamped(hp.value, 0, hp.max) / size;
+    const valueColorPct = Math.clamped(hp.value, 0, hp.max) / hp.max;
+
+    draw.background()
+        .current(valuePct, Color.forValue(valueColorPct))
+        .temp(tempPct)
+        .mainBorder();
+  }
+}
